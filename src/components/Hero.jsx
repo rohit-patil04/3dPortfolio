6eee1baf18513ed03feecae7a6b64b0e1f0270e7
Swastik <swastikyadav1237@gmail.com>
2025-06-13 19:47:42 +0530

    ..

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,14 +8,14 @@ const Hero = () => {
   return (
     <section className="relative w-full   h-screen mx-auto">
       <div
-        className={`${styles.paddingX} inset-0 sm:top-[10px] top-[60px] absolute max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} inset-0 sm:top-[10px] top-[30px] absolute max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
           <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
+          <h1 className={`${styles.heroHeadText} text-white `}>
             Hi, I'm <span className="text-[#915eff]">Swastik</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white`}>
