59b34a809e170c7307cbe9632840a76ed174b31d
Swastik <swastikyadav1237@gmail.com>
2025-06-18 10:00:45 +0530

    ..

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,35 +2,59 @@ const logo =
   "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/logo.png?updatedAt=1749961157002";
 const backend =
   "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/backend.png?updatedAt=1749961127387";
-const creator = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/creator.png?updatedAt=1749961125453";
-const mobile = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/mobile.png?updatedAt=1749961116254";
-const web = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/web.png?updatedAt=1749961126214";
-const github = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/github.png?updatedAt=1749965006566";
-const menu = "./menu.svg";
-const close = "./close.svg";
+const creator =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/creator.png?updatedAt=1749961125453";
+const mobile =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/mobile.png?updatedAt=1749961116254";
+const web =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/web.png?updatedAt=1749961126214";
+const github =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/github.png?updatedAt=1749965006566";
+import menu from "./menu.svg";
+import close from "./close.svg";
 
-const css = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/css.png?updatedAt=1749961104488";
-const docker = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/docker.png?updatedAt=1749961103144";
-const figma = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/figma.png?updatedAt=1749961103288";
-const git = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/git.png?updatedAt=1749961104346";
-const html = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/html.png?updatedAt=1749961104418";
-const javascript = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/javascript.png?updatedAt=1749961104755";
-const mongodb = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/mongodb.png?updatedAt=1749961104590";
-const nodejs = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/nodejs.png?updatedAt=1749961104709";
-const reactjs = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/reactjs.png?updatedAt=1749961105057";
-const redux = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/redux.png?updatedAt=1749961111710";
-const tailwind = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/tailwind.png?updatedAt=1749961111586";
-const typescript = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/typescript.png?updatedAt=1749961112627";
-const threejs = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/threejs.svg?updatedAt=1749961112674";
+const css =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/css.png?updatedAt=1749961104488";
+const docker =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/docker.png?updatedAt=1749961103144";
+const figma =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/figma.png?updatedAt=1749961103288";
+const git =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/git.png?updatedAt=1749961104346";
+const html =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/html.png?updatedAt=1749961104418";
+const javascript =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/javascript.png?updatedAt=1749961104755";
+const mongodb =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/mongodb.png?updatedAt=1749961104590";
+const nodejs =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/nodejs.png?updatedAt=1749961104709";
+const reactjs =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/reactjs.png?updatedAt=1749961105057";
+const redux =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/redux.png?updatedAt=1749961111710";
+const tailwind =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/tailwind.png?updatedAt=1749961111586";
+const typescript =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/typescript.png?updatedAt=1749961112627";
+const threejs =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tech/threejs.svg?updatedAt=1749961112674";
 
-const meta = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/company/meta.png?updatedAt=1749961112288";
-const shopify = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/company/shopify.png?updatedAt=1749961112545";
-const starbucks = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/company/starbucks.png?updatedAt=1749961112741";
-const tesla = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/company/tesla.png?updatedAt=1749961112753";
+const meta =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/company/meta.png?updatedAt=1749961112288";
+const shopify =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/company/shopify.png?updatedAt=1749961112545";
+const starbucks =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/company/starbucks.png?updatedAt=1749961112741";
+const tesla =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/company/tesla.png?updatedAt=1749961112753";
 
-const carrent = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/carrent.png?updatedAt=1749961151562";
-const jobit = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/jobit.png?updatedAt=1749961150488";
-const tripguide = "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tripguide.png?updatedAt=1749961172334";
+const carrent =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/carrent.png?updatedAt=1749961151562";
+const jobit =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/jobit.png?updatedAt=1749961150488";
+const tripguide =
+  "https://ik.imagekit.io/bqzlidc77g/my%20portfolio/tripguide.png?updatedAt=1749961172334";
 
 export {
   logo,
